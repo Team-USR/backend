e0aa538d42460404039338a1a69388557d2482f0
Vlad Stoica <vlad96stoica@gmail.com>
2017-02-06 18:57:11 +0000

    add rubocop

--- db/migrate/20170205141537_create_question.rb
+++
@@ -1,9 +0,0 @@
-class CreateQuestion < ActiveRecord::Migration[5.0]
-  def change
-    create_table :questions do |t|
-      t.timestamps
-      t.string :question, null: false
-      t.belongs_to :quiz
-    end
-  end
-end
