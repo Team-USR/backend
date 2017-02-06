918b5f1a35084316c85642f84a94594a8a8f8676
alexclp <alexandru.clapa@gmail.com>
2017-02-06 18:27:05 +0000

    Commented test & factory

--- spec/factories/quiz.rb
+++ spec/factories/quiz.rb
@@ -1,23 +1,23 @@
-FactoryGirl.define do
-  factory :quiz do
-    sequence(:title) { |n| "question#{n}" }
-  end
-
-  factory :question do
-    sequence(:question) { |n| "question#{n}" }
-    quiz FactoryGirl.create(:quiz)
-    trait :with_answers do
-      after(:create) do |question|
-        create(:answer, is_correct: true, question: question)
-        create(:answer, is_correct: false, question: question)
-        create(:answer, is_correct: false, question: question)
-      end
-    end
-  end
-
-  factory :answer do
-    sequence(:answer) { |n| "answer#{n}" }
-    is_correct false
-    question FactoryGirl.create(:question)
-  end
-end
+# FactoryGirl.define do
+#   factory :quiz do
+#     sequence(:title) { |n| "question#{n}" }
+#   end
+#
+#   factory :question do
+#     sequence(:question) { |n| "question#{n}" }
+#     quiz FactoryGirl.create(:quiz)
+#     trait :with_answers do
+#       after(:create) do |question|
+#         create(:answer, is_correct: true, question: question)
+#         create(:answer, is_correct: false, question: question)
+#         create(:answer, is_correct: false, question: question)
+#       end
+#     end
+#   end
+#
+#   factory :answer do
+#     sequence(:answer) { |n| "answer#{n}" }
+#     is_correct false
+#     question FactoryGirl.create(:question)
+#   end
+# end
