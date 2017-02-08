62fd7df18a137e010ee7f3788753bb7660b40643
alexclp <alexandru.clapa@gmail.com>
2017-02-08 12:52:28 +0000

    Fixed question spec

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,8 +6,8 @@ class Question < ApplicationRecord
   validates_presence_of :question
 
   def check_answer(answer_id)
-    answer = Answer.find(answer_id)
-    return false if answer.question_id != id
+    answer = Answer.find_by(id: answer_id)
+    return false if answer.nil? || answer.question_id != id
     answer.is_correct
   end
 end
