4e8baeb6c6ec5ee4ff9084ccf7331583cf2beb73
Vlad Stoica <vlad96stoica@gmail.com>
2017-02-23 21:39:08 +0000

    Refactor questions spec

--- spec/factories/quiz.rb
+++ spec/factories/quiz.rb
@@ -10,21 +10,32 @@ FactoryGirl.define do
     quiz
 
     factory :single_choice_question, class: Questions::SingleChoice do
-      trait :with_answers do
-        after(:create) do |question|
+      transient do
+        answers_count 0
+      end
+
+      after(:create) do |question, evaluator|
+        if evaluator.answers_count.positive?
           create(:answer, is_correct: true, question: question)
-          create(:answer, is_correct: false, question: question)
-          create(:answer, is_correct: false, question: question)
+          (1..evaluator.answers_count).each do
+            create(:answer, is_correct: false, question: question)
+          end
         end
       end
     end
 
     factory :multiple_choice_question, class: Questions::MultipleChoice do
-      trait :with_answers do
-        after(:create) do |question|
+      transient do
+        answers_count 0
+      end
+
+      after(:create) do |question, evaluator|
+        if evaluator.answers_count.positive?
           create(:answer, is_correct: true, question: question)
-          create(:answer, is_correct: true, question: question)
-          create(:answer, is_correct: false, question: question)
+          create(:answer, is_correct: false, question: question) if evaluator.answers_count > 1
+          (2..evaluator.answers_count).each do
+            create(:answer, is_correct: [true, false].sample, question: question)
+          end
         end
       end
     end
--- spec/models/questions/single_choice_spec.rb
+++ spec/models/questions/single_choice_spec.rb
@@ -1,20 +1,47 @@
 require 'rails_helper'
-require 'factory_girl_rails'
 
 RSpec.describe Questions::SingleChoice, type: :model do
-  subject { FactoryGirl.create(:single_choice_question, :with_answers) }
+  subject { create(:single_choice_question, answers_count: 4) }
 
   describe '#check' do
+    let(:correct_answer) { subject.answers.find_by(is_correct: true) }
+    let(:incorrect_answer) { subject.answers.find_by(is_correct: false) }
+
     it 'returns false if the id of the answer is not the correct one' do
       expect(subject.check({
-        answer_id: 1
-      })[:correct]).to eq(true)
+        answer_id: correct_answer.id
+      })).to eq({
+        correct: true,
+        correct_answer: correct_answer.id
+      })
     end
 
     it 'returns true if the id of the answer is the correct one' do
       expect(subject.check({
-        answer_id: 0
-      })[:correct]).to eq(false)
+        answer_id: incorrect_answer.id
+      })).to eq({
+        correct: false,
+        correct_answer: correct_answer.id
+      })
+    end
+  end
+
+  describe "#has_only_one_correct_answer" do
+    subject { create(:single_choice_question) }
+    it "marks the question as invalid if there's no correct answer" do
+      create(:answer, is_correct: false, question: subject)
+      create(:answer, is_correct: false, question: subject)
+      create(:answer, is_correct: false, question: subject)
+      create(:answer, is_correct: false, question: subject)
+      expect(subject.reload.valid?).to eq(false)
+    end
+
+    it "marks the question as invalid if there are multiple correct answer" do
+      create(:answer, is_correct: true, question: subject)
+      create(:answer, is_correct: false, question: subject)
+      create(:answer, is_correct: true, question: subject)
+      create(:answer, is_correct: false, question: subject)
+      expect(subject.reload.valid?).to eq(false)
     end
   end
 end
