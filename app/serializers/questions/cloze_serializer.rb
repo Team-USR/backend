0249e0648d63b0dc9d1e0fbc76fc77a66f9e249b
mihneamandache <mihnea.mandache@yahoo.com>
2017-02-23 16:52:00 +0000

    Finished JCloze feature

--- app/serializers/questions/cloze_serializer.rb
+++ app/serializers/questions/cloze_serializer.rb
@@ -2,6 +2,6 @@ class Questions::ClozeSerializer < ActiveModel::Serializer
   attributes :id, :question, :type, :sentence
 
   def sentence
-    object.cloze_sentence
+    object.cloze_sentence.text
   end
 end
