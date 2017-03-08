08812c3f7e58c0d33b8697654144b8ef538d27ae
mihneamandache <mihnea.mandache@yahoo.com>
2017-03-08 21:17:03 +0000

    Modified group tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170308195912) do
+ActiveRecord::Schema.define(version: 20170308205219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,10 +88,11 @@ ActiveRecord::Schema.define(version: 20170308195912) do
   end
 
   create_table "quizzes", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "title",      null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.string   "title",                      null: false
     t.integer  "user_id"
+    t.boolean  "published",  default: false, null: false
     t.index ["user_id"], name: "index_quizzes_on_user_id", using: :btree
   end
 
