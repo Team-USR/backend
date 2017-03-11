fa67543b938381115c0fce73f26d7128610821ff
Vlad Stoica <vlad96stoica@gmail.com>
2017-03-11 09:51:01 +0000

    Destroy quiz method

--- spec/controllers/quizzes_controller_spec.rb
+++ spec/controllers/quizzes_controller_spec.rb
@@ -586,4 +586,22 @@ RSpec.describe QuizzesController, type: :controller do
       end
     end
   end
+
+  describe "#destroy" do
+    let(:user) { create(:user) }
+    let(:quiz) { create(:quiz, user: user) }
+    let(:params) { { id: quiz.id } }
+
+    before do
+      authenticate_user user
+      quiz.questions << create(:single_choice_question)
+      quiz.questions << create(:single_choice_question)
+    end
+
+    it "destroys the quiz and all associated records" do
+      expect { delete :destroy, params }
+        .to change { Quiz.count }.by(-1)
+        .and change { Questions::SingleChoice.count }.by(-2)
+    end
+  end
 end
