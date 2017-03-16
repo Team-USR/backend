e87ab810ef3d37db87ecfa1effe6646c2aa10aab
Vlad Stoica <vlad96stoica@gmail.com>
2017-03-16 12:40:33 +0000

    Serializer fix

--- app/controllers/my_quizzes_serializer.rb
+++ app/controllers/my_quizzes_serializer.rb
@@ -2,7 +2,7 @@ class MyQuizzesSerializer < ActiveModel::Serializer
   attributes :id, :title, :status
 
   def status
-    session = QuizSession.find_by(quiz: Quiz.where(id: object.id), user: current_user)
+    session = QuizSession.where(quiz: Quiz.where(id: object.id), user: current_user).last
     if !session.nil?
       session.state
     else
