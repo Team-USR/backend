724f14f5fe4ec01b0e237ddf2ed2cfc26a5d8d48
alexclp <alexandru.clapa@gmail.com>
2017-03-17 20:38:32 +0000

    Fixed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20170316192944) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,6 +44,8 @@ ActiveRecord::Schema.define(version: 20170316192944) do
     t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_groups_on_user_id", using: :btree
   end
 
   create_table "groups_quizzes", id: false, force: :cascade do |t|
@@ -64,6 +65,7 @@ ActiveRecord::Schema.define(version: 20170316192944) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "gap_id"
+    t.index ["gap_id"], name: "index_hints_on_gap_type_and_gap_id", using: :btree
   end
 
   create_table "match_defaults", force: :cascade do |t|
@@ -158,4 +160,5 @@ ActiveRecord::Schema.define(version: 20170316192944) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
   end
 
+  add_foreign_key "groups", "users"
 end
