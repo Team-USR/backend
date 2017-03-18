8113fc9d9ed4affab9b47c36d3eab0d907a2e3c7
alexclp <alexandru.clapa@gmail.com>
2017-03-18 16:11:52 +0000

    Removed past function

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -9,10 +9,4 @@ class StatisticsController < ApplicationController
     .map { |g| { group_id: g.id, group_name: g.name, average: g.quizzes_average } }
     render json: averages
   end
-
-  private
-
-  def to_reject(quizzes)
-    quizzes.reject { |q| !q.quiz_sessions.where(state: "submitted").map(&:score).size.zero? }
-  end
 end
