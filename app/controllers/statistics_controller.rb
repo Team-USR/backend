102953b044cfaea2f768fcb118c24703c109e677
alexclp <alexandru.clapa@gmail.com>
2017-03-18 13:50:45 +0000

    Method to get avg

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -2,6 +2,18 @@ class StatisticsController < ApplicationController
   before_action :authenticate_user!
 
   def average_marks_groups
-    GroupsUser.where(user: current_user, role: "admin")
+    groups = current_user
+      .groups_users
+      .where(role: "admin")
+      .map { |e| Group.find(e.group_id) }
+    points = []
+    groups.each do |g|
+      points << g.quizzes
+                .sessions.where(state: "submitted")
+                .average(&:points)
+    end
+    render json: {
+      points: points
+    }
   end
 end
