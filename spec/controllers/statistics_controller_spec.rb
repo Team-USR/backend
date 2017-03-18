c26bc338d96091ddf2cf5a7e3cf50b00e725c4f3
alexclp <alexandru.clapa@gmail.com>
2017-03-18 16:18:21 +0000

    More tests

--- spec/controllers/statistics_controller_spec.rb
+++ spec/controllers/statistics_controller_spec.rb
@@ -4,14 +4,18 @@ RSpec.describe StatisticsController, type: :controller do
   describe "GET #average_marks_groups" do
     let(:user) { create(:user) }
     let(:grp) { create(:group, admin: user) }
+    let(:grp2) { create(:group, admin: user) }
     let(:g_u) { create(:groups_users, group: grp, user: user, role: "admin") }
+    let(:g_u2) { create(:groups_users, group: grp2, user: user, role: "admin") }
     let(:quiz) { create(:quiz, user: user, attempts: 2) }
+    let(:quiz2) { create(:quiz, user: user) }
     let(:session1) { create(:quiz_session, user: user, quiz: quiz, score: 5, state: "submitted") }
     let(:session2) { create(:quiz_session, user: user, quiz: quiz, score: 6, state: "submitted") }
 
     before do
       authenticate_user user
       grp.quizzes << quiz
+      grp2.quizzes << quiz2
       quiz.quiz_sessions << session1
       quiz.quiz_sessions << session2
     end
@@ -24,6 +28,11 @@ RSpec.describe StatisticsController, type: :controller do
             "group_id" => grp.id,
             "group_name" => grp.name,
             "average" => (session1.score + session2.score) / 2
+          },
+          {
+            "group_id" => grp2.id,
+            "group_name" => grp2.name,
+            "average" => nil
           }
         ]
       )
