045e2aef8ba07d418984fb7c4796a23b2aab0bca
Vlad Stoica <vlad96stoica@gmail.com>
2017-03-20 19:41:34 +0000

    Sidekiq

--- app/jobs/group_invite_job.rb
+++ app/jobs/group_invite_job.rb
@@ -1,5 +1,5 @@
 class GroupInviteJob < ApplicationJob
-  def perform(invite)
-    GroupInvite.create!(group: invite.group, email: invite.email)
+  def perform(group, email)
+    GroupInvite.create!(group_id: group.id, email: email)
   end
 end
