f1022fde3c6da113e5a0ea0b38aa91977d4ea0df
Vlad Stoica <vlad96stoica@gmail.com>
2017-03-21 18:09:00 +0000

    users search controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,34 @@
 class UsersController < ApplicationController
+  api :GET, '/groups/search', "Searches the users"
+  param :input, String, required: true, desc: "Search query"
+  example <<-EOS
+    {
+      "best_match_name": [
+        {
+          "id": 1,
+          "name": "input"
+        }
+      ],
+      "best_match_email": [
+        {
+          "id": 2,
+          "name": "exactlyinput@gmail.com"
+        }
+      ],
+      "alternative_match_name": [
+        {
+          "id": 3,
+          "name": "John input a"
+        }
+      ],
+      "alternative_match_name": [
+        {
+          "id": 3,
+          "name": "inputbb@gmail.com"
+        }
+      ]
+    }
+  EOS
   def search
     @best_match_name = User.where(name: params[:input])
     @best_match_email = User.where(email: params[:input])
