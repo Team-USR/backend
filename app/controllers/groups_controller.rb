812787239989d989f7458eb9b6fec732f07354f5
Vlad Stoica <vlad96stoica@gmail.com>
2017-03-21 18:03:00 +0000

    Add documentation for groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,23 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!
 
+  resource_description do
+    formats ['json']
+    error 401, "Need to be logged in"
+    error 401, "Unauthorized! Can't access resource"
+  end
+
+  api :GET, '/groups/:id/edit', "Return required attributes for editting a group"
+  param :id, :number, required: true, desc: "ID of group"
+  error 404, "Couldn't find group"
   def edit
     render json: GroupSerializer.new(Group.find(params[:id]), scope: "edit").as_json
   end
 
+  api :POST, '/groups', "Creates a group attached to the logged in user"
+  param :group, Hash, desc: "Details of group" do
+    param :name, String, required: true, desc: "Name of group required"
+  end
   def create
     @group = Group.new(params[:group])
     @group.groups_users.build(user_id: current_user.id, role: "admin")
@@ -15,6 +28,11 @@ class GroupsController < ApplicationController
     end
   end
 
+  api :POST, '/groups/:id/add', "Adds an user to the group"
+  param :id, :number, required: true, desc: "ID of group"
+  param :user_id, :number, required: true, desc: "ID of user"
+  error 404, "Couldn't find group"
+  error 404, "Couldn't find user"
   def add
     @group = Group.find(params[:id])
     authorize! :manage, @group
@@ -28,6 +46,11 @@ class GroupsController < ApplicationController
     end
   end
 
+  api :DELETE, '/groups/:id/delete', "Removes an user from the group"
+  param :id, :number, required: true, desc: "ID of group"
+  param :user_id, :number, required: true, desc: "ID of user"
+  error 404, "Couldn't find group"
+  error 404, "User is not in the group"
   def delete
     @group = Group.find(params[:id])
     authorize! :manage, @group
@@ -35,16 +58,34 @@ class GroupsController < ApplicationController
     @group.users.delete(@user_group.user)
   end
 
+  api :POST, '/groups/:id/users_update', "Updates (overrides) the users of the group"
+  param :id, :number, required: true, desc: "ID of group"
+  param :users, Array, of: String, required: true, desc: "List of emails"
+  error 404, "Couldn't find group"
+  example <<-EOS
+    [
+      {
+        "email": "vlad@kcl.ac.uk",
+        "status": "added"
+      },
+      {
+        "email": "vlad@kcl.ac.uk",
+        "status": "invited_to_join"
+      },
+    ]
+  EOS
+  description <<-EOS
+    Overrides users with the users received (admins are kept and can't be removed).
+    The response contains information about whether the user was added or invited
+    to join application if they do not have an account
+  EOS
   def users_update
     @group = Group.find(params[:id])
     authorize! :manage, @group
 
     @users = []
 
-    # We can't remove admins
-    @user_emails = params[:users]
-
-    @users_status = @user_emails.map do |user_email|
+    @users_status = @params.require(:users).map do |user_email|
       if user = User.find_by_email(user_email)
         @users << user
         {
@@ -65,6 +106,9 @@ class GroupsController < ApplicationController
     render json: @users_status, status: :ok
   end
 
+  api :DELETE, '/groups/:id', "Deletes the group"
+  param :id, :number, required: true, desc: "ID of group"
+  error 404, "Couldn't find group"
   def destroy
     @group = Group.find(params[:id])
     authorize! :manage, @group
@@ -72,12 +116,25 @@ class GroupsController < ApplicationController
     head :ok
   end
 
+  api :GET, '/groups/:id/quizzes', "Returns the quizzes from the group"
+  param :id, :number, required: true, desc: "ID of group"
+  error 404, "Couldn't find group"
   def quizzes
     @group = Group.find(params[:id])
     authorize! :display, @group
     render json: @group.quizzes, each_serializer: QuizIndexSerializer
   end
 
+  api :POST, '/groups/:id/quizzes_update', "Updates (overrides) the quizzes of the group"
+  param :id, :number, required: true, desc: "ID of group"
+  param :quizzes, Array, of: Integer, required: true, desc: "List of quizzes by id"
+  error 404, "Couldn't find group"
+  error 404, "Couldn't find quiz"
+  description <<-EOS
+    Overrides users with the users received (admins are kept and can't be removed).
+    The response contains information about whether the user was added or invited
+    to join application if they do not have an account
+  EOS
   def quizzes_update
     @group = Group.find(params[:id])
     authorize! :manage, @group
@@ -86,16 +143,37 @@ class GroupsController < ApplicationController
     head :ok
   end
 
+  api :GET, '/groups/:id/students', "Returns the students from the group"
+  param :id, :number, required: true, desc: "ID of group"
+  error 404, "Couldn't find group"
   def students
     @group = Group.find(params[:id])
     authorize! :manage, @group
     render json: @group.students, each_serializer: UserGetSerializer
   end
 
+  api :GET, '/groups/search', "Searches the groups"
+  param :input, String, required: true, desc: "Search query"
+  example <<-EOS
+    {
+      "best_match_name": [
+        {
+          "id": 1,
+          "name": "input"
+        }
+      ],
+      alternative_match_name: [
+        {
+          "id": 2,
+          "name": "something that contains input somewhere"
+        }
+      ]
+    }
+  EOS
   def search
-    @best_match_name = Group.where(name: params[:input])
+    @best_match_name = Group.where(name: params.require(:input))
 
-    @alternative_match_name = Group.where('name LIKE ?', "%#{params[:input]}%").all
+    @alternative_match_name = Group.where('name LIKE ?', "%#{params.require(:input)}%").all
       .reject{ |match| @best_match_name.include? match }
       .first(25)
 
--- app/controllers/test_secured_controller.rb
+++
@@ -1,7 +0,0 @@
-class TestSecuredController < ApplicationController
-  before_action :authenticate_user!
-
-  def test
-    render json: { data: "It worked" }
-  end
-end
