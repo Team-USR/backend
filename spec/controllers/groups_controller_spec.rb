48c7ae48c0636b01118e8c3205914bea455252c6
Vlad Stoica <vlad96stoica@gmail.com>
2017-03-23 13:42:40 +0000

    Case insensitive group search

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -351,6 +351,21 @@ RSpec.describe GroupsController, type: :controller do
       )
     end
 
+    it "returns the right best_match_name even if different case" do
+      post :search, params: { input: group.name.upcase }, as: :json
+      expect(JSON.parse(response.body)).to eq(
+        {
+          "best_match_name" => [
+            {
+              "id" => group.id,
+              "name" => group.name
+            }
+          ],
+          "alternative_match_name" => []
+        }
+      )
+    end
+
     it "returns the right alternative_match_name" do
       post :search, params: { input: group.name.chop }, as: :json
       expect(JSON.parse(response.body)).to eq(
