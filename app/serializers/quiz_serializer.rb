9fb57f5e5d563eb76de98f4ec5e9863cd2adbd64
Vlad Stoica <vlad96stoica@gmail.com>
2017-03-25 11:37:22 +0000

    Negative marking in quiz serializer

--- app/serializers/quiz_serializer.rb
+++ app/serializers/quiz_serializer.rb
@@ -1,5 +1,5 @@
 class QuizSerializer < ActiveModel::Serializer
-  attributes :id, :title, :creator, :published, :attempts, :release_date
+  attributes :id, :title, :creator, :published, :attempts, :release_date, :negative_marking
   has_many :questions
 
   def creator
