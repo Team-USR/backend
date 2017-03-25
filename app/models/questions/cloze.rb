bb0b6d7513616b0fc4ec54c23eae156c602703b8
Vlad Stoica <vlad96stoica@gmail.com>
2017-03-25 12:33:13 +0000

    validate presence of cloze sentence

--- app/models/questions/cloze.rb
+++ app/models/questions/cloze.rb
@@ -1,7 +1,7 @@
 class Questions::Cloze < Question
   belongs_to :quiz
   has_many :gaps, -> { order(:created_at) }, inverse_of: :question, as: :question, dependent: :destroy
-  has_one :cloze_sentence, inverse_of: :question, as: :question, dependent: :destroy
+  has_one :cloze_sentence, inverse_of: :question, as: :question, dependent: :destroy, required: true
   accepts_nested_attributes_for :gaps, :cloze_sentence, allow_destroy: true
 
   def gap_order
