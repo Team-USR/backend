148c60b1c15fb9472d8a2f7686a248363219a3f1
alexclp <alexandru.clapa@gmail.com>
2017-03-28 02:03:07 +0100

    Check if array is empty

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -201,8 +201,12 @@ class GroupsController < ApplicationController
   def quizzes_update
     @group = Group.find(params[:id])
     authorize! :manage, @group
-    @quizzes = params.require(:quizzes).map { |id| Quiz.find(id) }.uniq
-    @group.update!(quizzes: @quizzes)
+    if !params[:quizzes].empty?
+      @quizzes = params.require(:quizzes).map { |id| Quiz.find(id) }.uniq
+      @group.update!(quizzes: @quizzes)
+    else
+      @group.quizzes.destroy_all
+    end
     head :ok
   end
 
